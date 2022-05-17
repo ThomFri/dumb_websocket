b6ffd8bb3d4fd9a0e18fbc63f0de9542ad05ecb1
Thom Fri <thom.fri@gmx.de>
2022-05-17 10:53:42 +0200

    Some improvements ;)

--- app.py
+++ app.py
@@ -1,47 +1,149 @@
-from flask import Flask
+from asyncio import sleep
+
+from flask import Flask, send_from_directory
 from flask_sock import Sock
 from flask_socketio import SocketIO
 import json
+import copy
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 socketio = SocketIO(app, async_mode=None)
 sock = Sock(app)
 
-@sock.route('/cbws')
-def answer(sock):  # put application's code here
-    while True:
-        # get user input from browser
-        request_data = sock.receive()
-        print(f'Request received: "{str(request_data)}"')
+to_hide_keys = ["actions", "actions_data", "intents", "intents_data"]
 
-        try:
-            request_data = json.loads(request_data)
-        except Exception as e:
-            pass #no json!
+def convert_json(data, make_json=True):
+    was_json = False
+    try:
+        data = json.loads(data)
+        was_json = True
+    except Exception as e:
+        pass  # no json!
 
-        if isinstance(request_data, dict) and "message" in request_data:
-            user_message = request_data.get("message")
-        else:
-            user_message = request_data
-
-
-        ints = {"unknown": 1.00}
-        answer = f'Message "{user_message}" received!'
-
-        response_data = {
-            "message":  answer,
-            "intents": ints,
-            "further_data": []
+    if not was_json and make_json:
+        data = {
+            "message": data
         }
 
-        print(f'Response to send: "{str(response_data)}"')
+    return data, was_json
 
-        # send response to browser
-        response_data_str = json.dumps(response_data)
-        sock.send(response_data_str)
+
+def get_intentions(data):
+    intentions = {}
+    intentions_data = {}
+    user_message = data.get("message")
+
+    if "close" in user_message:
+        intentions["close"] = 1.0
+    elif "lookup" in user_message:
+        intentions["lookup"] = 1.0
+    else:
+        intentions["unknown"] = 1.0
+
+    intentions = {k: v for k, v in sorted(intentions.items(), key=lambda item: item[1], reverse=True)}
+    return intentions, intentions_data
+
+
+def get_answer(data, intents):
+    user_message = data.get("message")
+    return f'Message "{user_message}" received!'
+
+
+def get_actions(data, intents):
+    actions = []
+    actions_data = {}
+
+    if "lookup" in intents:
+        actions.append("lookup")
+        actions_data["lookup"] = {}
+
+    return actions, actions_data
+
+
+def get_events(data, intents):
+    events = []
+    events_data = {}
+
+    if "close" in intents:
+        events.append("close")
+
+    return events, events_data
+
+
+def get_further_data(data, intents):
+    return []
+
+@app.route('/')
+def display_chat():
+    return f'WebSocket implementation only; Get GUI from <a href="https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/ThomFri/dumb_websocket/tree/master/static">GitHub</a>'
+
+@sock.route('/cbws')
+def receive_input(sock):  # put application's code here
+    while True:
+        # get input data
+        request_data = sock.receive()
+        print(f'Request received: "{str(request_data)}"')
+        evaluate_input(sock, request_data)
+
+
+def evaluate_input(sock, request_data):
+    # evaluate received data
+    request_data, is_json = convert_json(request_data)
+    intents, intents_data = get_intentions(request_data)
+    actions, actions_data = get_actions(request_data, intents)
+    events, events_data   = get_events(request_data, intents)
+    answer                = get_answer(request_data, intents)
+    further_data          = get_further_data(request_data, intents)
+
+    # send response to browser
+    response_data = {
+        "message": answer,
+        "intents": intents,
+        "intents_data": intents_data,
+        "actions": actions,
+        "actions_data": actions_data,
+        "events": events,
+        "events_data": events_data,
+        "further_data": further_data
+    }
+    perform_actions(sock, response_data)
+
+
+def lookup(sock):
+    sleep(3) # not working yet!
+    data = {
+        "message": "I looked it up! ;)",
+        "events": [],
+        "events_data": {},
+        "further_data": []
+    }
+
+    send_to_user(sock, data)
+    pass
+
+
+def perform_actions(sock, data):
+    if "lookup" in data["actions"]:
+        lookup(sock)
+
+    send_to_user(sock, data)
+
+
+def send_to_user(sock, data):
+    data = copy.deepcopy(data) # copy dict to clean it
+    for to_hide_key in to_hide_keys:
+        try:
+            del data[to_hide_key]
+        except Exception as e:
+            pass # key wasn't present
+
+    print(f'Data to send: "{str(data)}"')
+    data_str = json.dumps(data)
+    sock.send(data_str)
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0', port=3000)
 
--- static/chat.js
+++ static/chat.js
@@ -1,8 +1,27 @@
 window.onload = function() {
-    const ws_url = 'ws://localhost:3000/cbws';
+    const ws_url = findGetParameter("ws_url") //'ws://localhost:3000/cbws';
     const chat_form = document.getElementById('chat_form');
     const chat_field = document.getElementById('chat_field');
     const chat_log = document.getElementById('chat_log');
+
+    const ws_prefix =  "WS: ";
+    const ws_class = "ws_message";
+    const user_prefix = "You: ";
+    const user_class = "you_message";
+
+    // helper for get param from https://stackoverflow.com/a/5448595
+    function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
 
     // init WebSocket
     const socket = new WebSocket(ws_url);
@@ -15,6 +34,7 @@ window.onload = function() {
     socket.onerror = function(evt) {
         alert("WS error; please reload page!");
     };
+
 
     // EventListener to check for answer
     socket.onmessage = function(evt) {
@@ -35,7 +55,7 @@ window.onload = function() {
             message =  response_data;
         }
 
-        append_to_chat('CB Message: ' + message, "red");
+        append_to_chat(ws_prefix + message, ws_class);
     };
 
 
@@ -45,7 +65,7 @@ window.onload = function() {
 
         // get user input
         var user_input = chat_field.value;
-        append_to_chat('User Input: ' + user_input, "blue");
+        append_to_chat(user_prefix + user_input, user_class);
 
         // create request data
         request_data = {
@@ -61,8 +81,9 @@ window.onload = function() {
         chat_field.value = '';
     };
 
-    const append_to_chat = (text, color) => {
-        chat_log.innerHTML += `<span style="color: ${color}">${text}</span><br>`;
+    // Append text to chat (with class)
+    const append_to_chat = (text, html_class) => {
+        chat_log.innerHTML += `<div class="${html_class}">${text}</div>`;
     };
 }
 
