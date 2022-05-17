63a6e2a29d9c17f4cf12459cfdc465fdda802004
Thom Fri <thom.fri@gmx.de>
2022-05-17 14:30:43 +0200

    Code cleaning

--- static/chat.js
+++ static/chat.js
@@ -1,14 +1,14 @@
 // html element vars
-    var ele_ws_status;
-    var ele_ws_url;
-    var ele_ws_json;
-    var ele_chat_form;
-    var ele_chat_field;
-    var ele_chat_log;
+    let ele_ws_status;
+    let ele_ws_url;
+    let ele_ws_json;
+    let ele_chat_form;
+    let ele_chat_field;
+    let ele_chat_log;
 
 // ws vars
-    var socket;
-    var ws_url;
+    let socket;
+    let ws_url;
 
 // chat vars
     const chat_ws_prefix =  "WS: ";
@@ -16,17 +16,19 @@
     const chat_user_prefix = "You: ";
     const chat_user_class = "you_message";
 
+// debug vars
+    const verbose = 1;
 
 // helper function for sleep
     const sleep = ms => new Promise(res => setTimeout(res, ms));
 
 // helper for get param from https://stackoverflow.com/a/5448595
     function findGetParameter(parameterName) {
-        var result = null,
+        let result = null,
             tmp = [];
 
         location.search
-            .substr(1)
+            .substring(1)
             .split("&")
             .forEach(function (item) {
                 tmp = item.split("=");
@@ -35,30 +37,64 @@
         return result;
     }
 
+// init everything after page load
+    window.onload = async function() {
+        // get html elements
+        if (verbose > 0) {
+            console.log("Getting HTML elements");
+        }
+        ele_ws_status = document.getElementById('ws_status');
+        ele_ws_url = document.getElementById('ws_url');
+        ele_ws_json = document.getElementById('ws_json');
+        ele_chat_form = document.getElementById('chat_form');
+        ele_chat_field = document.getElementById('chat_field');
+        ele_chat_log = document.getElementById('chat_log');
+
+        // get ws url from url
+        ws_url = findGetParameter("ws_url")
+        if (ws_url == null) {
+            console.warn("ws_url not defined as GET param!");
+            alert('Please specify ws_url as GET param! (e.g. "chat.html?ws_url=ws://localhost:3000/cbws"');
+        }
+        ele_ws_url.innerHTML = ws_url;
+
+        // connect ws
+        ele_ws_status.innerHTML = `WS is connecting...`;
+        ele_ws_status.style.backgroundColor = 'orange';
+        await ws_init();
+
+        // submit user input
+        ele_chat_form.onsubmit = event => {
+            submit_message(event);
+        };
+    };
 
 // init WebSocket
     async function ws_init(){
         while (socket === undefined || socket.readyState === 0 || socket.readyState === 3) {
             socket = new WebSocket(ws_url);
-            socket.onopen = function(evt) {
+            socket.onopen = function() {
+                if(verbose>0){console.log("WS connected!");}
                 ele_ws_status.innerHTML = `WS connected`;
                 ele_ws_status.style.backgroundColor = 'greenyellow';
             };
 
             //check if connection is ok
             while(socket.readyState === 0) {
-                //waiting for connection ;)
-                //console.log("Waiting for WS connection")
+                // waiting for connection
+                if(verbose>0){console.log("Waiting for WS connection");}
                 await sleep(500);
             }
         }
 
-        socket.onclose = function(evt) {
+        socket.onclose = function() {
+            if(verbose>0){console.warn("WS closed!");}
             ele_ws_status.innerHTML = `WS closed; reconnecting.. (if unsuccessful, please reload page!)`;
             ele_ws_status.style.backgroundColor = 'red';
             ws_init();
         };
-        socket.onerror = function(evt) {
+        socket.onerror = function() {
+            if(verbose>0){console.warn("WS error!");}
             ele_ws_status.innerHTML = `WS error; reconnecting.. (if unsuccessful, please reload page!)`;
             ele_ws_status.style.backgroundColor = 'red';
             socket.close();
@@ -66,23 +102,24 @@
         };
         // EventListener to check for answer
         socket.onmessage = function(event) {
+            if(verbose>1){console.log("WS received string:");console.log(event.data);}
             handle_message(event);
         };
     }
 
-
 // function for receiving chat
     function handle_message(event){
-        var response_data = event.data;
+        let response_data = event.data;
         try {
             response_data = JSON.parse(response_data);
         }
         catch (e){
-            console.log("response_data is not valid json")
+            console.warn("response_data is not valid json!")
         }
+        if(verbose>0){console.log("WS received message:");console.log(response_data);}
 
         ele_ws_json.innerHTML = event.data;
-        var message = "";
+        let message;
 
         if("message" in response_data) {
             message =  response_data["message"];
@@ -99,17 +136,19 @@
         event.preventDefault();
 
         // get user input
-        var user_input = ele_chat_field.value;
+        let user_input = ele_chat_field.value;
         append_to_chat(chat_user_prefix + user_input, chat_user_class);
 
         // create request data
-        request_data = {
+        let request_data = {
             "message": user_input,
             "other_stuff": null
         };
 
         // send user input to Socketserver
-        var request_data_str = JSON.stringify(request_data);
+        if(verbose>0){console.log("Sending message to WS:");console.log(JSON.stringify(request_data));}
+        let request_data_str = JSON.stringify(request_data);
+        if(verbose>1){console.log("Sending string to WS:");console.log(request_data_str);}
         socket.send(request_data_str);
 
         // reset textField to empty value
@@ -120,32 +159,3 @@
     const append_to_chat = (text, html_class) => {
         ele_chat_log.innerHTML += `<div class="${html_class}">${text}</div>`;
     };
-
-
-// init everything after page load
-    window.onload = function() {
-    // get html elements
-        ele_ws_status = document.getElementById('ws_status');
-        ele_ws_url = document.getElementById('ws_url');
-        ele_ws_json = document.getElementById('ws_json');
-        ele_chat_form = document.getElementById('chat_form');
-        ele_chat_field = document.getElementById('chat_field');
-        ele_chat_log = document.getElementById('chat_log');
-
-    // get ws url from url
-        ws_url = findGetParameter("ws_url") //'ws://localhost:3000/cbws';
-        if(ws_url == null) {
-            alert('Please specify ws_url as GET param! (e.g. "chat.html?ws_url=ws://localhost:3000/cbws"');
-        }
-        ele_ws_url.innerHTML = ws_url;
-
-    // connect ws
-        ele_ws_status.innerHTML = `WS is connecting...`;
-        ele_ws_status.style.backgroundColor = 'orange';
-        ws_init();
-
-    // submit user input
-        ele_chat_form.onsubmit = event => {
-            submit_message(event);
-        };
-}
